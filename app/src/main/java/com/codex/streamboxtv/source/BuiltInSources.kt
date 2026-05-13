d02fd5552b34315e7ada3266606fe16ec305828c
oscarxu27569 <oscarxu2259@gmail.com>
2026-05-13 22:19:22 +1000

    Replace built-in subscriptions with Migu source

--- app/src/main/java/com/codex/streamboxtv/source/BuiltInSources.kt
+++ app/src/main/java/com/codex/streamboxtv/source/BuiltInSources.kt
@@ -5,20 +5,8 @@ import com.codex.streamboxtv.data.SourceSubscription
 object BuiltInSources {
     val defaultSubscriptions = listOf(
         SourceSubscription(
-            name = "BurningC4 大陆优化订阅",
-            url = "https://iptv.burningc4.com/TV-IPV4.m3u",
-        ),
-        SourceSubscription(
-            name = "big-mouth-cn/tv 国内备用订阅",
-            url = "https://gh-proxy.com/https://raw.githubusercontent.com/big-mouth-cn/tv/main/iptv-ok.m3u",
-        ),
-        SourceSubscription(
-            name = "big-mouth-cn/tv 可用订阅",
-            url = "https://raw.githubusercontent.com/big-mouth-cn/tv/main/iptv-ok.m3u",
-        ),
-        SourceSubscription(
-            name = "BurningC4 GitHub 订阅",
-            url = "https://raw.githubusercontent.com/BurningC4/Chinese-IPTV/master/TV-IPV4.m3u",
+            name = "咪咕直播订阅",
+            url = "https://78962588856486165751857.iepose.cn/",
         ),
     )
 
